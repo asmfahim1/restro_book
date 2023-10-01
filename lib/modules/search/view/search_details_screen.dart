67acbd7adda46076bd0bdec52863ba78e245b3c3
asmfahim <asmfahim1@gmail.com>
2023-10-01 17:15:24 +0600

    sliver appbar added

--- lib/modules/search/view/search_details_screen.dart
+++ lib/modules/search/view/search_details_screen.dart
@@ -1,48 +1,119 @@
 import 'package:flutter/material.dart';
-import 'package:restro_book/core/utils/asset_path.dart';
 import 'package:restro_book/core/utils/colors.dart';
 import 'package:restro_book/core/utils/exports.dart';
 import 'package:restro_book/core/widgets/exports.dart';
 
-class SearchScreenDetails extends StatelessWidget {
+class SearchScreenDetails extends StatefulWidget {
   const SearchScreenDetails({Key? key}) : super(key: key);
 
+  @override
+  State<SearchScreenDetails> createState() => _SearchScreenDetailsState();
+}
+
+class _SearchScreenDetailsState extends State<SearchScreenDetails> {
+  List<String> assets = [
+    'assets/images/img1.png',
+    'assets/images/img2.png',
+    'assets/images/img4.png',
+    'assets/images/img5.png'
+  ];
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: [
+        slivers: <Widget>[
           SliverAppBar(
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            elevation: 0,
+            snap: false,
             pinned: true,
-            stretch: true,
-            leading: Icon(Icons.arrow_back_outlined, color: primaryColor,),
-            actions: const [
-              Padding(padding: EdgeInsets.symmetric(horizontal: 10), child: Icon(Icons.bookmark_outline_outlined, color: primaryColor,),),
-              Padding(padding: EdgeInsets.symmetric(horizontal: 10), child: Icon(Icons.share, color: primaryColor),),
-            ],
-            expandedHeight: 300,
+            floating: false,
             flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset(loginBackgroundImage, fit: BoxFit.cover,),
-              title: TextWidget('STK - San Fransisco', style: TextStyles.title16.copyWith(color: blackColor),),
-            ),
-          ),
-          SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 2,
-            ),
+                title: TextWidget('Flexible Spacebar', style: TextStyles.title16,), //Text
+                background: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: PageView.builder(
+                    itemCount: assets.length,
+                    physics: const BouncingScrollPhysics(),
+                    reverse: true,
+                    controller:
+                    PageController(initialPage: 3),
+                    itemBuilder: (context, index) {
+                      return Stack(
+                        children: [
+                          Stack(
+                            children: [
+                                Image.asset(
+                                assets[index],
+                                fit: BoxFit.cover,
+                              ),
+                              Positioned(
+                                left: size.width / 3,
+                                right: size.width / 3,
+                                bottom: 20,
+                                child: Container(
+                                  height: 30,
+                                  width: 50,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      color: Colors.black54
+                                  ),
+                                  child: TextWidget('See all 405 photos', style: TextStyles.regular12.copyWith(color: whiteColor),),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+            ), //FlexibleSpaceBar
+            expandedHeight: 300,
+            backgroundColor: whiteColor,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_outlined),
+              tooltip: 'Menu',
+              onPressed: () {},
+            ), //IconButton
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: IconButton(
+                  icon: const Icon(Icons.bookmark_outline_outlined),
+                  tooltip: 'Comment Icon',
+                  onPressed: () {},
+                ),
+              ), //IconButton
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: IconButton(
+                  icon: const Icon(Icons.share_outlined),
+                  tooltip: 'Setting Icon',
+                  onPressed: () {},
+                ),
+              ), //IconButton
+            ],//<Widget>[]
+          ), //SliverAppBar
+          SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                return Text('Text$index');
-              },
-            ),
-          ),
-        ],
-      ),
+                  (context, index) => ListTile(
+                tileColor: (index % 2 == 0) ? Colors.white : Colors.green[50],
+                title: Center(
+                  child: Text('$index',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 50,
+                          color: Colors.greenAccent[400]) //TextStyle
+                  ), //Text
+                ), //Center
+              ), //ListTile
+              childCount: 51,
+            ), //SliverChildBuildDelegate
+          ) //SliverList
+        ], //<Widget>[]
+      )
     );
   }
 }
