c271ab5d0c651907c06277bdc34e9fdc7f35cc9f
Abu Saleh Mohammad Fahim <36271104+ASM-FAHIM@users.noreply.github.com>
2023-10-15 10:38:40 +0600

    Update tabbar_widget.dart

--- lib/modules/search/view/widgets/tabbar_widget.dart
+++ lib/modules/search/view/widgets/tabbar_widget.dart
@@ -3,14 +3,49 @@ import 'package:restro_book/core/utils/exports.dart';
 import 'package:restro_book/core/widgets/exports.dart';
 
 class TabWidget extends StatelessWidget {
-  const TabWidget({Key? key}) : super(key: key);
+  final int indexNo;
+  const TabWidget({
+    this.indexNo = 1,
+    Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextWidget(
-      "Reservations",
-      style: TextStyles.regular12
-          .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
-    );
+    if(indexNo == 0){
+      return TextWidget(
+        'Reservation',
+        style: TextStyles.regular12
+            .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
+      );
+    }else if(indexNo == 1){
+      return TextWidget(
+        'Deliver & Takeout',
+        style: TextStyles.regular12
+            .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
+      );
+    }else if(indexNo == 2){
+      return TextWidget(
+        'Menu',
+        style: TextStyles.regular12
+            .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
+      );
+    }else if(indexNo == 3){
+      return TextWidget(
+        'Review',
+        style: TextStyles.regular12
+            .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
+      );
+    }else if(indexNo == 4){
+      return TextWidget(
+        'Details',
+        style: TextStyles.regular12
+            .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
+      );
+    }else{
+      return TextWidget(
+        'Reservation',
+        style: TextStyles.regular12
+            .copyWith(fontWeight: FontWeight.bold, fontSize: 13),
+      );
+    }
   }
 }
